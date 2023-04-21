5410695eb90d9db0f0471a4e56b3141f06316895
Eric Stiens <estiens@users.noreply.github.com>
2023-04-21 09:32:18 -0500

    add basic prompt spec

--- app/prompts/prompt_builder.rb
+++ app/prompts/prompt_builder.rb
@@ -3,22 +3,47 @@ class PromptBuilder
     @last_actions = last_actions
     @goal = goal
     @custom_prompts = custom_prompts
+    @summary = summary
   end
 
   def worker_prompt
-    "#{prompt_start}\n#{prompt_commands}\n#{prompt_workspace}\nYour last few actions: #{@last_actions.join(',')}\nIf you received relevant information from your memories, you dont have to repeat the command.\n#{prompts['prompt_end']}"
+    prompt = "#{prompt_start}\n#{prompt_commands}\n#{prompt_workspace}\n"
+    prompt += "Your last few actions: #{@last_actions.join(',')}"
+    prompt += "Read this prompt carefully to see if it already has what you need.\n"
+    if @summary
+      prompt += "This is a summary of your last action: #{@summary}, it may have advice in it\n"
+    end
+    prompt += "#{prompts['prompt_end']}\n"
+    prompt += @custom_prompts.join("\n")
+    prompt
   end
 
+  private
+
   def prompt_start
-    "#{prompts['prompt_start']}\nYour overarching goal is: #{@goal}."
+    prompt = "#{prompts['prompt_start']}\nYour overarching goal is: #{@goal}.\n"
+    work_instructions = "#{Config.app_directory}/worker_directory/worker_instructions.txt"
+    prompt += "You have instructions you should read at #{work_instructions}.\n"
+    prompt += "There is also available_commands.yml that lists all the commands you can use.\n"
+    prompt += "You can also use shell commands.\n"
+    prompt += "You cannot make up your own commands.\n"
+    prompt += 'There is also a file called learnings.txt where you should write down anything'
+    prompt += 'that you want to remember. You are forgetful, so I will use that to remind you.'
+    prompt
   end
 
   def prompt_commands
-    prompts['command_prompt'] + "\n"
+    "#{prompts['command_prompt']}\n"
   end
 
   def prompt_workspace
-    "#{prompts['workspace_prompt']}/nYour workspace is at #{Config.workspace_path}. Your workspace currently contains: #{Dir.entries(Config.workspace_path)}"
+    prompt = "#{prompts['workspace_prompt']}/nYour workspace is at #{Config.workspace_path}.\n"
+    prompt += "Your workspace currently contains: #{Dir.entries(Config.workspace_path)}\n"
+    learnings = File.read(File.open(
+                            "#{Config.app_directory}/worker_instructions/learnings.txt", 'r'
+                          ))
+    prompt += "You also wanted to remember the following things: #{learnings}"
+    prompt
   end
 
   def prompts
---
+++ spec/prompts/prompt_builder_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+RSpec.describe PromptBuilder, type: :model do
+  describe '#worker_prompt' do
+    let(:custom_prompts) { ['Custom prompt 1', 'Custom prompt 2'] }
+    let(:goal) { 'Test goal' }
+    let(:summary) { 'Test summary' }
+    let(:last_actions) { ['Action 1', 'Action 2'] }
+    let(:prompt_builder) { PromptBuilder.new(custom_prompts: custom_prompts, goal: goal, summary: summary, last_actions: last_actions) }
+
+    it 'includes custom prompts' do
+      custom_prompts.each do |prompt|
+        expect(prompt_builder.worker_prompt).to include(prompt)
+      end
+    end
+
+    it 'includes goal' do
+      expect(prompt_builder.worker_prompt).to include(goal)
+    end
+
+    it 'includes summary' do
+      expect(prompt_builder.worker_prompt).to include(summary)
+    end
+
+    it 'includes last actions' do
+      expect(prompt_builder.worker_prompt).to include(last_actions.join(','))
+    end
+  end
+end
